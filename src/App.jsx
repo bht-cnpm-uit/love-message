64fdd30740e94cb9713e40e03c2106fa051c6d26
Lam <127223170+lamuitit@users.noreply.github.com>
2024-01-23 00:05:18 +0700

    update
    create file src/components./TagMessage.jsx and folder Assets to restore img

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+
+import TagMessage from "./components/TagMessage";
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return ( 
+        <TagMessage />
+    )
 }
