470ddf5affed6ffa80053d9dec3b3bd328a00640
bmhungqb <21522110@gm.uit.edu.vn>
2024-01-29 13:08:57 +0700

    use onSnapshot

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import Background from '../../src/Assets/header.png';
-import { FaEllipsisV } from 'react-icons/fa';
-import { collection, getDocs } from 'firebase/firestore';
-import { app, db, auth } from '../config/firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
+import {db } from '../config/firebase';
 import BoxCreateMessage from './BoxCreateMessage';
 import Footer from './Footer';
 import AddMessageTest from './AddMessageTest';
 import ButtonCreateMessage from './ButtonCreateMessage';
 import TagMessage from './TagMessage';
-
 const Layout = () => {
     const [isOpenCreateMessage, setIsOpenCreateMessage] = useState(false);
     const [scrollY, setScrollY] = useState(0);
     const [messages, setMessages] = useState([]);
     const dbMessages = collection(db, 'messages');
 
-    const getData = async () => {
-        const data = await getDocs(dbMessages);
-        const messageData = data.docs.map((doc) => ({
-            id: doc.id, // Lấy key của document
-            ...doc.data(), // Lấy dữ liệu của document
-        }));
-        setMessages(messageData);
-    };
+    useEffect(() => {
+        // Subscribe to the 'messages' collection using onSnapshot
+        const unsubscribe = onSnapshot(dbMessages, (snapshot) => {
+            const messageData = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setMessages(messageData);
+        });
+
+        // Cleanup function to unsubscribe when the component unmounts
+        return () => {
+            unsubscribe();
+        };
+    }, []); 
 
     useEffect(() => {
         const handleScroll = () => {
@@ -35,9 +40,6 @@ const Layout = () => {
         };
     }, []);
 
-    useEffect(() => {
-        getData();
-    }, []);
     return (
         <>
             <header
@@ -58,7 +60,6 @@ const Layout = () => {
                         <AddMessageTest
                             isOpenCreateMessage={isOpenCreateMessage}
                             setIsOpenCreateMessage={setIsOpenCreateMessage}
-                            getData={getData}
                         />
                     </div>
                 </div>
