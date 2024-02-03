1cf6db0c7d0067962d872232f4ffbd1ed1801b6a
Lam <127223170+lamuitit@users.noreply.github.com>
2024-02-03 10:10:21 +0700

    fixed background-color border-color

--- src/components/BigTagMessage.jsx
+++ src/components/BigTagMessage.jsx
@@ -28,15 +28,16 @@ const BigTagMessage = ({
     };
 
     return (
-        <div className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100% - 1rem)] max-h-full">
+        <div className={``}>
+        <div className="flex font-bold overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100% - 1rem)] max-h-full">
             <div className="relative p-4 w-full max-w-md max-h-full">
-                <div className="relative bg-white border-4 border-yellow-300 rounded-lg shadow-md">
+                <div className={`${data.color.bg_color} ${data.color.border_color} relative  border-4  rounded-lg shadow-md`}>
                     <div className="p-4 md:p-5">
                         <form className="space-y-4">
                             <div className="mb-4">
                                 <div className="relative">
                                     <p
-                                        className="w-full border-b-2 text-center border-gray-300 py-2 px-3 text-gray-700 leading-tight"
+                                        className="w-full border-b-2 text-center border-gray-300 py-2 px-3 text-gray-700 leading-tight bg-white"
                                         style={{ fontFamily: 'Dancing Script' }}
                                     >
                                         {data.nickname}
@@ -47,9 +48,9 @@ const BigTagMessage = ({
                             <div className="mb-4">
                                 <div className="relative">
                                     <p
-                                        className="w-full border-b-2 border-gray-300 py-2 px-4 text-gray-700 leading-tight"
+                                        className="w-full border-b-2 border-gray-300 py-2 px-4 text-gray-700 leading-tight bg-white text-base"
                                         id="message"
-                                        style={{ fontFamily: 'Dancing Script' }}
+                                        style={{ fontFamily: 'monospace' }}
                                     >
                                         {data.message}
                                     </p>
@@ -100,6 +101,7 @@ const BigTagMessage = ({
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
--- src/components/DeleteMessage.jsx
+++ src/components/DeleteMessage.jsx
@@ -38,7 +38,7 @@ const DeleteMessage = ({ isOpenDeleteMessage, setIsOpenDeleteMessage, data}) => 
     return (
         <div className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100% - 1rem)] max-h-full">
         <div className="relative p-4 w-full max-w-md max-h-full">
-        <div className="relative bg-white border-4 border-yellow-300 rounded-lg shadow-md">
+        <div className={`relative bg-white border-4  rounded-lg shadow-md ${data.color.border_color}`}>
             <div className="p-4 md:p-5">
                 <form className="space-y-4">
                 <div className="mb-4">
