7f390360611af366f9cf7dcf3aa480de223c5eca
bmhungqb <21522110@gm.uit.edu.vn>
2024-02-06 21:05:38 +0700

    fix qr

--- src/components/QRComponent.jsx
+++ src/components/QRComponent.jsx
@@ -8,13 +8,13 @@ const QRComponent = ({ setIsOpenQR }) => {
                 Gửi lời nhắn đến mọi người thành công &#10084;&#65039;
             </p>
             <p className="text-black text-xl mb-4" style={{fontStyle: "italic"}}>
-                Quét QR để nhận lì xì từ BHTCNPM
+                Mã QR lì xì sẽ xuất hiện từ 0h-12h ngày 10/02(nhằm mùng 1 Tết)
             </p>
-            <img
+            {/* <img
                 src="src/Assets/1.png" // Thay đổi đường dẫn đến hình ảnh QR của bạn
                 alt="Lì xì"
                 style={{ maxWidth: '100%', height: 'auto' }}
-            />
+            /> */}
             <button
                 className="bg-[#B7AE91] hover:bg-[#A7A181] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 onClick={() => {
