687a2229fdff645b07bb74cffdfaaabf2ae43cfe
bmhungqb <21522110@gm.uit.edu.vn>
2024-02-06 16:30:32 +0700

    add new database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD09kNEOneN_dIhhI41btZQ8iNSr1rbaGs",
+//     authDomain: "love-message-ac796.firebaseapp.com",
+//     projectId: "love-message-ac796",
+//     storageBucket: "love-message-ac796.appspot.com",
+//     messagingSenderId: "870633465275",
+//     appId: "1:870633465275:web:c443b266772a778843109e",
+//     measurementId: "G-E2N3DTN46M"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyD09kNEOneN_dIhhI41btZQ8iNSr1rbaGs",
-    authDomain: "love-message-ac796.firebaseapp.com",
-    projectId: "love-message-ac796",
-    storageBucket: "love-message-ac796.appspot.com",
-    messagingSenderId: "870633465275",
-    appId: "1:870633465275:web:c443b266772a778843109e",
-    measurementId: "G-E2N3DTN46M"
+  apiKey: "AIzaSyA_YRmK2da7SLlnuS1npIyw2q7nEH22kEg",
+  authDomain: "love-message-3bff6.firebaseapp.com",
+  projectId: "love-message-3bff6",
+  storageBucket: "love-message-3bff6.appspot.com",
+  messagingSenderId: "464254606485",
+  appId: "1:464254606485:web:680880ed091ef681108f9c",
+  measurementId: "G-D6VENYS52Q"
 };
 
 export const app = initializeApp(firebaseConfig);
